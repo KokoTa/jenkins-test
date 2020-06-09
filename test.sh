5c659bf2a8fa1fa9409af51236fb214dc9170b8f
KokoTa <584847514@qq.com>
2020-06-09 19:36:34 +0800

    update: test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 ###
  # @Author: KokoTa
  # @Date: 2020-06-09 15:58:06
- # @LastEditTime: 2020-06-09 19:29:12
+ # @LastEditTime: 2020-06-09 19:36:31
  # @Description: 
 ### 
 
 echo 'hello'
-echo 'world'
