c41da01d4749505d2ec490897408da27bea371ce
KokoTa <584847514@qq.com>
2020-06-09 15:58:21 +0800

    feat: shell

---
+++ test.sh
@@ -0,0 +1,8 @@
+###
+ # @Author: KokoTa
+ # @Date: 2020-06-09 15:58:06
+ # @LastEditTime: 2020-06-09 15:58:07
+ # @Description: 
+### 
+
+echo 'hello'
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: KokoTa
+ * @Date: 2020-06-09 15:03:56
+ * @LastEditTime: 2020-06-09 15:05:42
+ * @Description:
+ */
+
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+    port: 8888
+  }
+}
