640652cabfb35f12a634f67b264f8b7154c2ba43
KokoTa <584847514@qq.com>
2020-06-10 09:32:43 +0800

    update: test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 ###
  # @Author: KokoTa
  # @Date: 2020-06-09 15:58:06
- # @LastEditTime: 2020-06-10 09:31:01
+ # @LastEditTime: 2020-06-10 09:32:36
  # @Description: 
 ### 
 
-echo 'hello123'
+echo 'hello'
+echo 'hello'
