4fdb342444f9cfd46532ad11f08365039ab190a7
KokoTa <584847514@qq.com>
2020-06-10 09:51:34 +0800

    update: 更新

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 ###
  # @Author: KokoTa
  # @Date: 2020-06-09 15:58:06
- # @LastEditTime: 2020-06-10 09:49:39
+ # @LastEditTime: 2020-06-10 09:51:23
  # @Description: 
 ### 
 
 echo 'hello world'
+echo '最后一次测试了，成功了就结束'
 echo '最后一次测试了，成功了就结束'
 echo '最后一次测试了，成功了就结束'
